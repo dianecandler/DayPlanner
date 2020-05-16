81e39647d63f9c5af69b27589e02b473303ed48a
Diane Candler <dianedcandler@gmail.com>
2020-05-15 20:27:41 -0500

    added HTML 8-5 hours and save buttons for input text using provided CSS - heavy documentation - works

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,17 +1,19 @@
 // utilizing JQuery
-$(document).ready(function () {
+
+// confirm document is ready JQuery, below is short hand for the following:
+// $( document ).ready(function() {
+//     console.log( 'ready!' );
+//   });
+$(function() {
+    console.log( 'ready!' );
+  });
+  
+// $(document).ready(function () {
 	// event listeners
 	// 	$('#remaining-time').hide();
 	// 	$('#start').on('click', quiz.startGame);
 	// 	$(document).on('click', '.option', quiz.checkAnswers);
 });
 
-// var quiz = {
-// 	// quiz properties
-// 	correct: 0,
-// 	incorrect: 0,
-// 	unanswered: 0,
-// 	currentScore: 0,
-// 	quizTimer: false,
-// 	timerId: '',
-// 	clock: 0,
+// define months
+
