05bf6d412b073667255ffb9c2b3ecd81d4712882
Diane Candler <dianedcandler@gmail.com>
2020-05-15 19:20:07 -0500

    file set-up

---
+++ Assets/scripts.js
@@ -0,0 +1,17 @@
+// utilizing JQuery
+$(document).ready(function () {
+	// event listeners
+	// 	$('#remaining-time').hide();
+	// 	$('#start').on('click', quiz.startGame);
+	// 	$(document).on('click', '.option', quiz.checkAnswers);
+});
+
+// var quiz = {
+// 	// quiz properties
+// 	correct: 0,
+// 	incorrect: 0,
+// 	unanswered: 0,
+// 	currentScore: 0,
+// 	quizTimer: false,
+// 	timerId: '',
+// 	clock: 0,
