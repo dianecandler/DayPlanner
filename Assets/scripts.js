4488825e12321b8f0c560a791dc80b8ab41593a8
Diane Candler <dianedcandler@gmail.com>
2020-05-16 14:41:11 -0500

    pushing minor edits, notes in file before tutoring

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -4,16 +4,23 @@
 // $( document ).ready(function() {
 //     console.log( 'ready!' );
 //   });
-$(function() {
-    console.log( 'ready!' );
-  });
-  
-// $(document).ready(function () {
-	// event listeners
+$(function () {
+	console.log('ready!');
+});
+
+$(document).ready(function () {
+	//run JQuery once HTML loaded
+mmoment("#currentDay").format('dddd', +', ','MMMM Do YYYY, h:mm:ss a'
+	// $('#currentDay'); //.? for date/time in top of page
+	// event listeners examples
 	// 	$('#remaining-time').hide();
 	// 	$('#start').on('click', quiz.startGame);
 	// 	$(document).on('click', '.option', quiz.checkAnswers);
-});
 
-// define months
-
+	// moment.locale(); // en
+	// moment().format('dddd'); // day of the week
+	// moment().format('MMMM Do YYYY, h:mm:ss a'); // May 16th 2020, 12:59:42 pm
+	// moment().format('LT'); //1:03pm
+	// moment().format('MMM Do YY');
+	// define months here
+}); //end JQuery here
