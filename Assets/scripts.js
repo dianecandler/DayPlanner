57981dc189b775af591ef3c1f3732221b1a648c5
Diane Candler <dianedcandler@gmail.com>
2020-05-18 18:39:33 -0500

    final pull before class

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,21 +1,18 @@
 // utilizing JQuery - could have defined buttons here but elected to put in HTML for increased stability
 $(document).ready(function () {
 	console.log('ready!'); //-- inside document function to test JQuery ready
+	moment.locale(); // en - US local format from https://momentjs.com/
 
 	// Global variable
-	const works = false;
-	moment.locale(); // en
-	//US local format from https://momentjs.com/
+	const works = false;  
+
 	var now = moment().format('LLLL'); // Monday, May 18, 2020, 12:20:40 am
 	console.log(now);
 
-	// test if non-standard hours - H for 24 hr, h for 12 hr in Moment, using nowH24 or 12 to determine - NEED TEXT BOX SELECTOR?
-	// 'let' defines varible only inside the function -  DELETE? colors work without this
-	let nowH24 = moment().format('H');
+	let nowH24 = moment().format('H');  //text if non-standard hours; H for 24 hr, h for 12 hr in Moment
 	let nowH12 = moment().format('h');
 
-	// define what is "past" or after hours using "works" variable
-	if (works) {
+	if (works) {  //define what is "past" or after hours using "works" variable
 		nowH24 = 13;
 		nowH12 = 1;
 	}
@@ -23,9 +20,7 @@ $(document).ready(function () {
 	let dateHeading = $('#dateAtTop');
 	dateHeading.text(now);
 
-	// make variables for time and date
-	//make variables for the current date and time
-	var rnn = new Date();
+	var rnn = new Date();  //create varibles for the current date and time
 	var currentHour = rnn.getHours(); //16>militarytime
 
 	//change the description box color based on time - use 18 military time
@@ -40,20 +35,19 @@ $(document).ready(function () {
 			document.getElementById(i.toString()).classList.add('future');
 		}
 		console.log([ currentHour ]);
+	}
 		//get items stored in local storage
-		var s = i.toString();
-		{
-			document.getElementById(s).value = localStorage.getItem(s);
-			console.log([ s ]);
-		}
-	}
+		// var s = i.toString();		{
+		// 	document.getElementById(s).value = localStorage.getItem(s);
+		// 	console.log([ s ]);
+		// }
+		//function to store text (Console says savetext is not defined)
+	// function savetext (i) {
+	// 	var myText = document.getElementById(i.toString()).value;
+	// 	localStorage.setItem(i.toString(), myText);
+	// 	console.log([ myText ]);
+	// }
 
-	//function to store text (Console says savetext is not defined)
-	function savetext (i) {
-		var myText = document.getElementById(i.toString()).value;
-		localStorage.setItem(i.toString(), myText);
-		console.log([ myText ]);
-	}
 	// WORKS ABOVE but NO STORAGE
 
 	// Get stored requests, meetings, input from localStorage
